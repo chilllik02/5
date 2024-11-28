38945f7bb488df9c0ba44c1e9c2d985363569465
User <User@SRV-PO>
2024-11-28 08:29:45 +0400

    Добавьте файлы проекта.

---
+++ 5/5.cpp
@@ -0,0 +1,38 @@
+﻿#include <locale.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    const int sotr = 12;
+
+    int sum = 0;
+
+
+    int newsotr[sotr];
+    int temp;
+    for (int i = 0; i < sotr; i++)
+    {
+        cout << "Введите зп " << i+1 << " сотрудника за март : ";
+        cin >> newsotr[i];
+        cout << "Введите зп " << i+1 << " сотрудника за апрель : ";
+        cin >> temp;
+        newsotr[i] += temp;
+        cout << "Введите зп " << i+1 << " сотрудника за май : ";
+        cin >> temp;
+        newsotr[i] += temp;
+        cout << "Зп сотрудника за весь квартал: "<< newsotr[i] << " рублей." << endl;
+        cout << endl;
+    }
+
+    for (int j = 0; j < sotr; j++)
+    {
+        sum += newsotr[j];
+    }
+    cout << "Сумма зп за весь квартал всех сотрудников составляет " << sum << " рублей." << endl;
+
+
+}
+
+
